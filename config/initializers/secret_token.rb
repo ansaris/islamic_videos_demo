8bcf052b7e02c8deb2854c1b7e9976bb30c5e642
Salman Ansari <salman.ansari.usa@gmail.com>
2013-11-03 18:26:07 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IslamicVideosDemo::Application.config.secret_key_base = '32e74bd66719ccd967c39e9ffa4abe2763aa1074e4671f5dfdb4e01921f4872556ffa349ee3fca6c2c0a68fd392b6d9c617ef138a948f52944ac52113379472b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IslamicVideosDemo::Application.config.session_store :cookie_store, key: '_islamic_videos_demo_session'
